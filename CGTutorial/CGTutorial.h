449d69a7406d1834550c25f383c0427c19d08dc9
Chase Gorectke <nbvikingsidiot001@gmail.com>
2014-03-29 18:39:16 -0400

    Initial commit.

---
+++ CGTutorial/CGTutorial.h
@@ -0,0 +1,13 @@
+//
+//  CGTutorial.h
+//  CGTutorial
+//
+//  Created by Chase Gorectke on 3/28/14.
+//  Copyright (c) 2014 Revision Works, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+#import "CGTutorialPanel.h"
+#import "CGTutorialView.h"
